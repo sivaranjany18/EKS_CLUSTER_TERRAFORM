fd44f2041c39c84157edee70ed142692621b8f5f
sivaranjany18 <sivaranjany18@gmail.com>
2026-02-16 16:55:24 +0530

    Change instance type from t2.medium to t3.micro

--- eks.tf
+++ eks.tf
@@ -36,7 +36,7 @@ module "eks" {
       max_size     = 2
       desired_size = 1
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.micro"]
       capacity_type  = "SPOT"
 
       tags = {
