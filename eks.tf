5d494506a862cb832b5bbe2cf069ff64e3e0c495
sivaranjany18 <sivaranjany18@gmail.com>
2026-02-16 20:10:11 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -24,7 +24,7 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    ami_type       = "UBUNTU_22_04_x86_64"
+    ami_type       = "AL2023_x86_64_STANDARD"
     instance_types = ["t3.small"]
 
     attach_cluster_primary_security_group = true
