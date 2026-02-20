f0d98016d23ce80009a4967848fd845b9a59e9fc
sivaranjany18 <sivaranjany18@gmail.com>
2026-02-20 11:03:17 +0530

    Change AWS region from ap-south-1 to us-east-1

--- provider.tf
+++ provider.tf
@@ -9,19 +9,19 @@ terraform {
   backend "s3" {
     bucket = "siva-terraform-state-2026"
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"   
+    region = "us-east-1"   
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 locals {
-  region = "ap-south-1"
-  name = "vgs_cluster"
+  region = "us-east-1"
+  name = "ssr_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-1a", "ap-south-1b"]
+  azs      = ["us-east-1a", "us-east-1a"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
